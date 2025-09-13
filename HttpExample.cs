46b63dccf4cb411a6cc26a93ba1c29f78da8304a
Aeezin <gullestrand@gmail.com>
2025-09-13 16:44:18 +0200

    Added initial project files and configurations for Azure Functions

---
+++ HttpExample.cs
@@ -0,0 +1,57 @@
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace My.Functions;
+
+public class HttpExample
+{
+    private readonly ILogger<HttpExample> _logger;
+
+    public HttpExample(ILogger<HttpExample> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HttpExample")]
+    public async Task<MultiResponse> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        FunctionContext executionContext
+    )
+    {
+        var logger = executionContext.GetLogger("HttpExample");
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var message = "Welcome to Azure Functions!";
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        await response.WriteStringAsync(message);
+
+        return new MultiResponse()
+        {
+            Document = new MyDocument { id = System.Guid.NewGuid().ToString(), message = message },
+            HttpResponse = response,
+        };
+    }
+}
+
+public class MultiResponse
+{
+    [CosmosDBOutput(
+        "my-database",
+        "my-container",
+        Connection = "CosmosDbConnectionSetting",
+        CreateIfNotExists = true
+    )]
+    public MyDocument Document { get; set; }
+    public HttpResponseData HttpResponse { get; set; }
+}
+
+public class MyDocument
+{
+    public string id { get; set; }
+    public string message { get; set; }
+}
